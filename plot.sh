e1ec020fb4f08ec9d1da136937bc16e50be778d2
joyling93 <963034741@qq.com>
2021-07-02 14:16:30 +0800

    1.1

--- plot.sh
+++ plot.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 source /PROJ/home/xiezhuoming/miniconda2/bin/activate /PROJ/home/xiezhuoming/miniconda2/envs/velocyto
 Rscript ./bin/velocyto.plot.R \
---velocyto_rds /PROJ/development/xiezhuoming/proj/velocyto/pkg/output/demo/velocyto.rds \
---seurat_obj2project /PROJ/development/xiezhuoming/proj/velocyto/seektest/velocyto/test_seurat.rds \
+--velocyto_rds /PROJ/development/xiezhuoming/proj/velocyto/output/demo/velocyto.rds \
+--seurat_obj2project /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/demo_seurat.rds \
 --outdir ./output \
---gene2show CCNL2,SSU72,lala,GNB1
+--gene2show CCNL2,SSU72,errortest,GNB1
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 source /PROJ/home/xiezhuoming/miniconda2/bin/activate /PROJ/home/xiezhuoming/miniconda2/envs/velocyto
 Rscript ./bin/velocyto.prep.R \
---bam /PROJ/development/xiezhuoming/proj/velocyto/pkg/input/demo/seekone_demo/demo.bam \
---filtered_barcode /PROJ/development/xiezhuoming/proj/velocyto/pkg/input/demo/seekone_demo/filtered_barcodes.tsv.gz \
+--bam /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/demo.bam \
+--filtered_barcode /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/filtered_barcodes.tsv.gz \
 --species human \
 --add_tag F \
 --outdir ./output \
