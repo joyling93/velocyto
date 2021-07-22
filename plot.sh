f4ce7259c85cc7c93a8a3afd59048caf2e6f8f60
joyling93 <64716661+joyling93@users.noreply.github.com>
2021-07-22 10:31:56 +0800

    conda path change

--- plot.sh
+++ plot.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-source /PROJ/home/xiezhuoming/miniconda2/bin/activate /PROJ/home/xiezhuoming/miniconda2/envs/velocyto
+source /PROJ/development/xiezhuoming/soft/miniconda2/bin/activate /PROJ/development/xiezhuoming/soft/miniconda2/envs/velocyto
 Rscript ./bin/velocyto.plot.R \
 --velocyto_rds /PROJ/development/xiezhuoming/proj/velocyto/output/demo/velocyto.rds \
 --seurat_obj2project /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/demo_seurat.rds \
