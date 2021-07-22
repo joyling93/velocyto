db2f68f29ae42cfd9d679c6025fe7a383e0459a4
joyling93 <64716661+joyling93@users.noreply.github.com>
2021-07-22 10:32:46 +0800

    conda path change

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 #--bam /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/demo.bam \
 #--filtered_barcode /PROJ/development/xiezhuoming/proj/velocyto/input/demo/seekone_demo/filtered_barcodes.tsv.gz \
-source /PROJ/home/xiezhuoming/miniconda2/bin/activate /PROJ/home/xiezhuoming/miniconda2/envs/velocyto
+source /PROJ/development/xiezhuoming/soft/miniconda2/bin/activate /PROJ/development/xiezhuoming/soft/miniconda2/envs/velocyto
 Rscript ./bin/velocyto.prep.R \
 --bam /PROJ/development/xiezhuoming/proj/velocyto/input/demo/10xdemo/outs/possorted_genome_bam.bam \
 --filtered_barcode /PROJ/development/xiezhuoming/proj/velocyto/input/demo/10xdemo/outs/filtered_feature_bc_matrix/barcodes.tsv.gz \
